c36d39faa913b46b234c8436b91ed926287d0d07
Chi Zhang <elecpaoao@gmail.com>
2013-08-12 10:53:23 +0800

    update

--- filter.js
+++ filter.js
@@ -1,14 +1,19 @@
-var feeds = $('.WB_feed_type');
-chrome.storage.local.get("filter_words" , function(result) {
-	var filter_words = result.filter_words;
-	for (var i = 0; i < feeds.length; i++) {
-	    var text = feeds[i].innerHTML;
-	    for (var j = 0; j < filter_words.length; j++) {
-	        var pattern = filter_words[j];
-	        if (text.match(pattern)) {
-		        console.log("hide: "+ pattern);
-	            $(feeds[i]).hide();
-	        }
-	    }
-	}
-});
+function do_filter() {
+    chrome.storage.local.get("filter_words" , function(result) {
+    	var filter_words = result.filter_words;
+    	var f = document.getElementsByClassName("WB_feed_type");
+    	for (var i = 0; i < f.length; i++) {
+    	    var text = $(f[i]).text();
+    	    for (var j = 0; j < filter_words.length; j++) {
+    	        var pattern = filter_words[j];
+    	        if (text.match(pattern)) {
+    		        console.log("hide: "+ pattern);
+    	            $(f[i]).hide();
+    	        }
+    	    }
+            
+    	}
+    });
+}
+
+setTimeout(do_filter, 3000);
